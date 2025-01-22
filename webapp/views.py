93512c62220c0227c7b6ea773a9f535ec4f36337
Phoenix Pegasus <tumashenkaaliaksandr@gmail.com>
2025-01-22 23:42:44 +0300

    added new model Album on the index template

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from webapp.models import Album
+
+
 # Create your views here.
 def index(request):
     """Main page music site ДяДя"""
-    return render(request, 'webapp/index.html')
+    music_album = Album.objects.all()
+    context = {
+        'music_album': music_album
+    }
+    return render(request, 'webapp/index.html', context=context)
 
 
 def albums(request):
